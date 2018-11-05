454d3b591bb1524585876382bd0620cda2bdef8f
COR Students <corstudents@Matts-MacBook-Pro-57.local>
2018-11-05 11:51:33 -0500

    done

---
+++ calculator.js
@@ -0,0 +1,33 @@
+
+// $(document).ready(function(){
+//     let firstNum = 0;
+//     let firstNumResult = 0;
+//     let isfirstNum = true;
+//     let secondNum = 0;
+//     let secondNumResult = 0;
+//     let issecondNum = true;
+
+//     $(".value").click(function(){
+//         firstNum = $(this).attr('value');
+//         console.log('first number is ' + firstNum);
+    
+//     $(".show").click(function(){
+//         $("#topBar").html(counter += 1) 
+//         let firstnum = e.target.innerHTML;
+//         if (show >= '0' && show <= '9') {
+//             handleNumber('number');
+//         } else {
+//             handleOperator('operator');
+    
+//         }
+//     });
+// });
+
+// $(document).ready(function(){
+//   $(".button").click(function(){
+    
+//   })
+
+
+// })
+// })
